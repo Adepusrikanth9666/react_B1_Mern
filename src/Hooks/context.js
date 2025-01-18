c3c1af344815a16a2df843c7e570a040fb5473c1
Swathi <arlapudiswathi03@gmail.com>
2025-01-18 07:47:15 +0530

    done

--- src/Hooks/context.js
+++ src/Hooks/context.js
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
-
-import { dataContext} from '../App'
+import { dataContext,MovieInfoContext} from '../App'
 
 function Context(){
 
     const Data= useContext(dataContext);
+    let movie=useContext(MovieInfoContext)
     return(
         <>
         <div> this is a context
         <h3>{JSON.stringify(Data)}</h3>
+        <h3>{JSON.stringify(movie)}</h3>
        </div>
        </>
        
--- src/Hooks/useContext.js
+++ src/Hooks/useContext.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 
 import {MovieInfoContext } from "../App"
+import Context from "./context";
 
 function Usecontext(){
     const moviveData = useContext(MovieInfoContext);
@@ -9,6 +10,7 @@ function Usecontext(){
         <div>
             THIS IS USECONTEXT
            <h1>{JSON.stringify(moviveData)}</h1>
+           <Context/>
         </div>
         </>
         
